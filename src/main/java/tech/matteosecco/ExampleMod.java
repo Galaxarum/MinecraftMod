277caf78a8518d3079f30fdbf6a20b3384266f17
Matteo Secco <matteo.secco97@gmail.com>
2021-03-10 13:10:05 +0100

    Added mod java file, teleport wand behaviour java file

---
+++ src/main/java/tech/matteosecco/ExampleMod.java
@@ -0,0 +1,32 @@
+package tech.matteosecco;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import tech.matteosecco.teleport_wand.TeleportWand;
+
+
+public class ExampleMod implements ModInitializer {
+
+	private static final String NAMESPACE = "matteosecco";
+	private static final FabricItemSettings WAND_SETTINGS = new FabricItemSettings()
+			.group(ItemGroup.TOOLS)
+			.fireproof()
+			.maxCount(1)
+			.maxDamage(0);
+
+	private static final Item WAND_BODY = new Item(new FabricItemSettings().group(ItemGroup.MISC).fireproof().maxDamage(0));
+	private static final Item MAGIC_WAND = new TeleportWand(WAND_SETTINGS);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		Registry.register(Registry.ITEM, new Identifier(NAMESPACE, "wand_body"), WAND_BODY);
+		Registry.register(Registry.ITEM, new Identifier(NAMESPACE, "teleport_wand"), MAGIC_WAND);
+	}
+}
---
+++ src/main/java/tech/matteosecco/teleport_wand/TeleportWand.java
@@ -0,0 +1,78 @@
+package tech.matteosecco.teleport_wand;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+
+public class TeleportWand extends Item {
+	public TeleportWand(Settings settings) {
+		super(settings);
+	}
+
+	private boolean usedOnBlock = false;
+
+
+	@Override
+	public ActionResult useOnBlock(ItemUsageContext context) {
+		final PlayerEntity player = context.getPlayer();
+		if(player!=null) {
+			setTeleportTarget(player, context.getStack());
+			usedOnBlock = true;
+			return ActionResult.SUCCESS;
+		}
+		return ActionResult.FAIL;
+	}
+
+	@Override
+	public TypedActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
+		final ItemStack wand = player.getStackInHand(hand);
+		if(usedOnBlock){
+			usedOnBlock = false;
+			return TypedActionResult.pass(wand);
+		}
+		executeTeleport(player, wand);
+		return TypedActionResult.success(wand);
+	}
+
+	@Override
+	public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+		return super.finishUsing(stack, world, user);
+	}
+
+	@Override
+	public void onCraft(ItemStack wand, World world, PlayerEntity player) {
+		setTeleportTarget(player,wand);
+	}
+
+	@Override
+	public boolean isEnchantable(ItemStack stack) {
+		return false;
+	}
+
+	@Override
+	public boolean isFireproof() {
+		return true;
+	}
+
+	private void setTeleportTarget(PlayerEntity player, ItemStack wand){
+		final Vec3d pos = player.getPos();
+		final CompoundTag target = wand.getOrCreateTag();
+		target.putDouble("targetX", pos.x);
+		target.putDouble("targetY", pos.y);
+		target.putDouble("targetZ", pos.z);
+		wand.setTag(target);
+	}
+
+	private void executeTeleport(PlayerEntity player, ItemStack wand){
+		final CompoundTag target = wand.getOrCreateTag();
+		player.updatePosition(target.getDouble("targetX"), target.getDouble("targetY"), target.getDouble("targetZ"));
+	}
+}
